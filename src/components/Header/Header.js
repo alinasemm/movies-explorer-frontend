97dc4d96f53c759a1a2593aca728e7f3a60d4c3a
Alina Semikolenova <alinasemmm@yandex.ru>
2021-06-14 15:02:40 +0300

    Сверстала блоки header и main

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import headerLogo from '../images/header-logo.svg';
+import headerLogo from '../../images/header-logo.svg';
 
 function Header() {
   return (
-    <header className="header">
+    <header className='header'>
       <img alt='логотип страницы о Проекте' src={headerLogo} className="header__logo"/>
+      <div className='header__content'>
         <span className='header__registration'>
           Регистрация
         </span>
-        <button className='header__button'>
+        <button className='header__button'>Войти</button>
       </div>
     </header>
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 function Main() {
   return (
     <div className="main">
+      <Header />
     </div>
   );
 }
