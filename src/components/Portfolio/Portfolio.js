b322c15c1c531266f70837c8698072f9c26ab15e
Alina Semikolenova <alinasemmm@yandex.ru>
2021-06-16 00:35:27 +0300

    Сверстала блоки Portfolio и AboutMe

---
+++ src/components/Portfolio/Portfolio.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import portfolioLinkIcon from '../../images/portfolio-link-icon.svg';
+
+function Portfolio() {
+  return (
+    <div className='portfolio'>
+      <h3 className='portfolio__tittle'>Портфолио</h3>
+      <div className='portfolio__links'>
+        <div className='portfolio__link'>
+          <a href='#' className='portfolio__link-name'>Статичный сайт</a>
+          <img className='portfolio__link-icon' alt='стрелка' src={portfolioLinkIcon} />
+        </div>
+        <div className='portfolio__link'>
+          <a href='#' className='portfolio__link-name'>Адаптивный сайт</a>
+          <img className='portfolio__link-icon' alt='стрелка' src={portfolioLinkIcon} />
+        </div>
+        <div className='portfolio__link'>
+          <a href='#' className='portfolio__link-name'>Одностраничное приложение</a>
+          <img className='portfolio__link-icon' alt='стрелка' src={portfolioLinkIcon} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
