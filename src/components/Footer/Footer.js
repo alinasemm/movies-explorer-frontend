1e803651d383848870ae9fcb67002c2e71d9058a
Alina Semikolenova <alinasemmm@yandex.ru>
2021-06-16 00:58:18 +0300

    Сверстала футер

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <h3 className='footer__header'>Учебный проект Яндекс.Практикум х BeatFilm.</h3>
+      <div className='footer__content'>
+        <span> &copy; 2020 </span>
+        <div className='footer__links'>
+          <span className='footer__link'>Яндекс.Практикум</span>
+          <span className='footer__link'>Facebook</span>
+          <span className='footer__link'>Github</span>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
