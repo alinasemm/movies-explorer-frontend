bd3355a8cae05113420fe2f2a57e910517278365
Alina Semikolenova <alinasemmm@yandex.ru>
2021-06-17 17:32:30 +0300

    Сделала верстку компонента SearchForm, начала верстать чекбокс для короткометражек

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SearchForm() {
+  return (
+    <div className="search-form">
+      <button className='search-form__write-movie-name' />
+      <h2 className='search-form__tittle'>Фильм</h2>
+      <button className='search-form__find-movie' />
+    </div>
+  );
+}
+
+export default SearchForm;
