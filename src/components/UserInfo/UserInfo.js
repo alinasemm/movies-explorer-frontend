5235f292db4c1ea4a11dd53f8bf3a7e2c764ec52
Alina Semikolenova <alinasemmm@yandex.ru>
2021-07-06 15:14:20 +0300

    Адаптация navigation и menu

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 function UserInfo ({ contClassName = '', textClassName = '', buttonClassName = '', text = '', buttonText = '' }) {
   return (
