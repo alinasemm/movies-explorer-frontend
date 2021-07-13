4b2eadd6ebb0917a2b733993292a7d8fee2498e1
Alina Semikolenova <alinasemmm@yandex.ru>
2021-07-13 19:24:31 +0300

    Доделала страницу ошибки для всех размеров страниц

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 function PageNotFound () {
   return (
     <div className='not-found'>
-      <h2 className='not-found__tittle'>404</h2>
-      <p className='not-found__text'>Страница не найдена</p>
+      <div className='not-found__content'>
+        <h2 className='not-found__tittle'>404</h2>
+        <p className='not-found__text'>Страница не найдена</p>
+      </div>
       <Link className="not-found__button" to="/">Назад</Link>
     </div>
   );
