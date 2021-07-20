a8110d24a1b9d3ce7f84a987cc8cf897a5af228f
Alina Semikolenova <alinasemmm@yandex.ru>
2021-07-20 16:31:40 +0300

    Сделала разметку для Button и Link для страницы регистрации

---
+++ src/components/Link/Link.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Link({ onClick, text, linkText }) { 
+  return (
+    <span className="link-text">
+      {text}
+      <span className='link-button' onClick={onClick}> {linkText}</span>
+    </span>
+  );
+}
+
+export default Link;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Form from '../Form/Form';
+import Button from '../Button/Button';
+import Link from '../Link/Link';
+import { useHistory } from 'react-router-dom';
 
 function Register() {  
+  const history = useHistory();
 
   return (
     <div className='register'>
       <Form
-      title='Добро пожаловать!'
+        title='Добро пожаловать!'
       >
+        <Button text='Зарегистрироваться'/>
+        <Link text='Уже зарегистрированы?' linkText='Войти' onClick={() => history.push('/signin')}/>
       </Form>
     </div>
   );
